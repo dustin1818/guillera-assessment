eb8b3fcbada77eb7e765b9fa6d6c1e6cf911eb6c
dustin1818 <dustinamoda18@gmail.com>
2025-03-03 22:56:58 +0800

    fix mobile responsive

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  navItems = [
+    { path: '/kanban', label: 'Kanban' },
+    { path: '/calendar', label: 'Calendar' },
+    { path: '/map', label: 'Map' }
+  ]
 }
